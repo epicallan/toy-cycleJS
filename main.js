ebdb7fd136892c6727dbe81f3da339f4513d6f17
allan <epicallan.al@gmail.com>
2016-12-24 13:45:50 +0300

    set up test

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-const hello = "hello world";
-console.log(hello);
+import Rx from 'rxjs/Rx';
+
+// logic
+function main() {
+  return Rx.Observable.timer(0, 1000)
+    .map(i => `seconds elapsed ${i}`);
+}
+
+// Dom effects
+function DomEffects(text$) {
+  text$.subscribe(text => {
+    const container = document.querySelector('#app');
+    container.textContent = text;
+  });
+}
+
+
+function consoleLogEffecr(msg$) {
+  msg$.subscribe(msg => console.log(msg));
+}
+
+const sinks = main(); // inputs
+
+DomEffects(sinks);
+consoleLogEffecr(sinks);
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-## Toy Version of cycleJS with xstreams and Rxjs
+## Toy Version of cycleJS with Rxjs and snabbdom for virtual DOM
 
-This repository is meant to explore the use of xstream and possibly rxjs in creating a toy  version of cycleJS a function frontend library.
+This repository is meant to explore the use of rxjs in creating a toy  version of cycleJS a function frontend library.
